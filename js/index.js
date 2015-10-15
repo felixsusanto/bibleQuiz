4ddbaa4e32743f7ace7fd5d9560a27a93eb111e7
felixsusanto <felix.susanto@wds.co>
2015-10-15 18:41:01 +0800

    major updates

--- js/index.js
+++ js/index.js
@@ -16,7 +16,24 @@ $(document).ready(function(){
   $container.imagesLoaded(function(){
     $container.isotope({
       itemSelector: '.element-item',
-      sortBy:'random'
+      sortBy:'order'
     });
+  });
+
+  //game mechanics
+  $('.multiplechoices').on('click',function(e){
+    var $this = $(this);
+    var originalScore = $this.prevAll('.score').data('score');
+    var totalChoices = $this.find('label').length;
+    var totalIncorrect = $this.find('label input.false:checked').length;
+    var calculate = totalChoices-totalIncorrect==1?0:Math.round(originalScore*(1-(totalIncorrect/(totalChoices-1))));
+    $($this.prevAll('.score').get(0)).text(calculate);
+  });
+  $('select[name="one"]').on('change', function() {
+    $(this).closest('.form-group').siblings('.info').children('.one').text( this.value );
+  });
+
+  $('select[name="two"]').on('change', function() {
+    $(this).closest('.form-group').siblings('.info').children('.two').text( this.value );
   });
 });
