24059ae1798ec2162df6e6beadf098282d985f1e
Felix Susanto <felix.susanto2000@gmail.com>
2015-11-10 00:03:27 +0800

    APIspark integration

--- js/index.js
+++ js/index.js
@@ -60,38 +60,56 @@ $(document).ready(function(){
   });
 
   //TIMER
-  var seconds = 60;
-
+  var timer;
   $("#start").on('click',function(){
     var init = 0;
-    $("#timer").countdown(Date.now() + seconds*1000, function(event) {
-      $(this).text(
-        event.strftime('%M:%S')
-      );
-    }).on("finish.countdown",function(e){
-      $(this).text(
-          "TIME'S UP!"
-        ).addClass('text-warning');
-    });;
-    $('.circle').circleProgress({
-      size:200,
-      value: 0,
-      lineCap: 'round',
-      fill: { color: '#ffa500' },
-      animation: { duration: seconds*1000 },
-      animationStartValue:1.0
+    $.ajax({
+      url: "https://amorebiblequiz.apispark.net/v2/variables/",
+      "content-type": 'application/JSON',
+      accept: 'application/JSON'
+    }).done(function(result) {
+      $( this ).addClass( "done" );
+      timer = result[0].Timer;
+      $("#timer").countdown(Date.now() + timer*1000, function(event) {
+        $(this).text(
+          event.strftime('%M:%S')
+        );
+      }).on("finish.countdown",function(e){
+        $(this).text(
+            "TIME'S UP!"
+          ).addClass('text-warning');
+      });;
+      $('.circle').circleProgress({
+        size:200,
+        value: 0,
+        lineCap: 'round',
+        fill: { color: '#ffa500' },
+        animation: { duration: timer*1000 },
+        animationStartValue:1.0
+      });
     });
   });
 
   //TIME LEFT
-  $(".timeleft").countdown(/*"2015/11/21 15:50:00"*/Date.now() + 10000, function(event) {
-    $(this).text(
-      event.strftime('%D days %H:%M:%S')
-    );
-  }).on("finish.countdown",function(e){
-    $(this).text(
-        "TIME'S UP!"
-      ).addClass('text-warning h1');
+  var deadline;
+  $.ajax({
+    url: "https://amorebiblequiz.apispark.net/v2/variables/",
+    "content-type": 'application/JSON',
+    accept: 'application/JSON'
+  }).done(function(result){
+    deadline = result[0].Foo; 
+    console.log(result);
+    $(".timeleft").countdown(deadline, function(event) {
+      $(this).text(
+        event.strftime('%D days %H:%M:%S')
+      );
+    }).on("finish.countdown",function(e){
+      $(this).text(
+          "TIME'S UP!"
+        ).addClass('text-warning h1');
+    });
   });
+
+  
 
 });
